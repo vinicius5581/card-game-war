104ebe053b3d76da2a789498e1e29f13598b6200
Vinicius Santana <santanaviny@gmail.com>
2021-01-17 20:25:44 -0800

    card getHTML method

--- deck.js
+++ deck.js
@@ -26,6 +26,18 @@ class Card {
     this.suit = suit
     this.value = value
   }
+
+  get color() {
+    return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red'
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement('div')
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add('card', this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
 
 const freshDeck = () => SUITS.flatMap(suit => VALUES.map(value => new Card(suit, value)))
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import Deck from './deck.js';
 
+const computerCardSlot = document.querySelector('.computer-card-slot');
+
+
 const deck = new Deck()
 
 console.log('fresh deck: ', [...deck.cards])
@@ -7,3 +10,5 @@ console.log('fresh deck: ', [...deck.cards])
 deck.shuffle()
 
 console.log('suffled deck: ', deck)
+
+computerCardSlot.appendChild(deck.cards[9].getHTML())
